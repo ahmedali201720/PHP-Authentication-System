8cbaf51cff42d721f8cbf6822fbf5a6517546b12
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2020-10-07 18:40:53 +0200

    separating database connection

--- inc/db.php
+++ inc/db.php
@@ -1,1 +1,15 @@
 
+<?php
+
+function dbConnection() {
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "AdminPanel";
+
+	$conn = new mysqli($server,$username,$password,$db);
+
+	return $conn;
+}
+
+?>
